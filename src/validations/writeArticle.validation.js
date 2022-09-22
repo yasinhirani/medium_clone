c248c49fc027d9573805689eebaf00a15404bbfd
Yasin Hirani <yasin.hirani@1rivet.local>
2022-09-22 11:41:37 +0530

    Add : Validation to handle empty white space in textbox and delete confirm modal to delete article

--- src/validations/writeArticle.validation.js
+++ src/validations/writeArticle.validation.js
@@ -1,12 +1,23 @@
 import * as Yup from "yup";
 
 const WriteArticleValidation = Yup.object({
-  title: Yup.string().required("Title is required"),
-  sub_title: Yup.string().required("Sub Title is required"),
+  title: Yup.string()
+    .required("Title is required")
+    .min(5, "Minimum 5 characters required")
+    .matches(/^[a-zA-Z\s/]*$/g, "Invalid title")
+    .trim(),
+  sub_title: Yup.string()
+    .required("Sub Title is required")
+    .min(5, "Minimum 5 characters required")
+    .matches(/^[a-zA-Z\s]*$/, "Invalid sub-title")
+    .trim(),
   content: Yup.array().of(
     Yup.object().shape({
       style: Yup.string(),
-      contentText: Yup.string().required("Content is required"),
+      contentText: Yup.string()
+        .required("Content is required")
+        .min(5, "Minimum 5 characters required")
+        .trim(),
     })
   ),
   category: Yup.string().required("Category is required"),
