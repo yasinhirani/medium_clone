a22c8aaf14aa211743b505639f772f51378ee08a
Yasin Hirani <yasin.hirani@1rivet.local>
2022-10-11 14:09:11 +0530

    Feat : Added section for articles filtering

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { useContext, useRef } from "react";
 import { Link } from "react-router-dom";
 import { MediumContext } from "../context/Context";
@@ -6,11 +7,34 @@ import Post from "./Post";
 
 const Home = () => {
   const { articles, articlesLoading } = useContext(MediumContext);
+  const [allArticles, setAllArticles] = useState({});
+  const [categoryList, setCategoryList] = useState([]);
+  const [filterText, setFilterText] = useState("");
   // console.log(articles);
   const articleRef = useRef(null);
   const goToArticle = () => {
     articleRef.current.scrollIntoView({ behavior: "smooth" });
   };
+
+  const handleFilter = (category) => {
+    setFilterText(category);
+    const filteredArticles =
+      articles &&
+      articles.filter((article) => article.data.category === category);
+    setAllArticles(filteredArticles);
+  };
+
+  const handleClearFilter = () => {
+    setFilterText("");
+    setAllArticles(articles);
+  };
+
+  useEffect(() => {
+    const list = [];
+    articles && articles.map((data) => list.push(data.data.category));
+    setCategoryList([...new Set(list)]);
+    setAllArticles(articles);
+  }, [articles]);
   return (
     <>
       <Banner goToArticle={goToArticle} />
@@ -32,11 +56,52 @@ const Home = () => {
             </p>
           </div>
         )}
-        {articles &&
-          !articlesLoading &&
-          articles.map((article) => (
-            <Post key={Math.random()} id={article.id} {...article.data} />
-          ))}
+        <div className="flex flex-col-reverse lg:flex-row items-start lg:space-x-10">
+          <div>
+            {articles &&
+              !articlesLoading &&
+              allArticles.length > 0 &&
+              allArticles.map((article) => (
+                <Post key={Math.random()} id={article.id} {...article.data} />
+              ))}
+          </div>
+          <div className="mb-8 lg:mb-0">
+            {categoryList && categoryList.length > 0 && (
+              <h5 className="font-semibold text-xl mb-4">Filter:-</h5>
+            )}
+            <div className="flex items-center space-x-5">
+              {categoryList &&
+                categoryList.length > 0 &&
+                categoryList.map((category) => {
+                  return (
+                    <button
+                      key={Math.random()}
+                      className={`${
+                        filterText === category
+                          ? "bg-gray-400 text-gray-800"
+                          : "bg-gray-200 text-gray-600"
+                      } text-xs font-semibold px-4 py-1.5 rounded-full`}
+                      onClick={() => {
+                        handleFilter(category);
+                      }}
+                    >
+                      <p>{category}</p>
+                    </button>
+                  );
+                })}
+            </div>
+            {filterText !== "" && (
+              <button
+                className="mt-4 bg-black bg-opacity-90 px-3 py-1 text-xs text-white rounded-xl"
+                onClick={() => {
+                  handleClearFilter();
+                }}
+              >
+                clear filter
+              </button>
+            )}
+          </div>
+        </div>
       </div>
     </>
   );
